f16b297bf48cf04ed458b22820993126a6b621a1
Bettina Santana de Meirelles <103582720+BettinaSM@users.noreply.github.com>
2026-03-21 21:37:28 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,10 @@ provider "local" {}
 
 resource "null_resource" "vm_simulation" {
   provisioner "local-exec" {
-    command = "echo Simulating VM creation..."
+    command = "echo VM created && sleep 1"
   }
+}
+
+output "server_ip" {
+  value = "127.0.0.1"
 }
